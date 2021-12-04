4821ca02a2ed18a9c920db8a77e5f6152d2440af
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-12-04 14:05:24 +0700

    drew the snake on the grid

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const startBtn = document.querySelector('#start');
 const scoreDisplay = document.getElementById('score');
 const grid = document.querySelector('.grid');
 let squares = [];
+theSnake = [2, 1, 0];
 
 function createGrid() {
   for (let i = 0; i < 100; i++) {
@@ -13,3 +14,11 @@ function createGrid() {
 }
 
 createGrid();
+
+theSnake.forEach((index) => squares[index].classList.add('snake'));
+
+// function move() {
+
+// }
+
+// move()
